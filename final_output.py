7e49f1d0d78f8946f25dc99b91c68904b2f9ec07
Aleksandr Glebov <112719934+aleksandrGlebov@users.noreply.github.com>
2023-06-29 16:31:30 -0600

    Add files via upload

---
+++ final_output.py
@@ -0,0 +1,45 @@
+# Function to format a line from product_spec_ids.txt
+def format_product_spec_id(spec_id):
+    if spec_id.startswith("FTR_"):
+        return f"\"gs://telus-kb-feature/test/{spec_id}.json\""
+    return spec_id
+
+# Function to format a line from updated_ids.txt
+def format_updated_id(updated_id):
+    if updated_id.startswith("O_"):
+        return f"\"gs://telus-kb-add-ons/{updated_id}.json\""
+    elif updated_id.startswith("R_"):
+        return f"\"gs://telus-kb-add-ons/{updated_id}.json\""
+    elif updated_id.startswith("G_"):
+        return f"\"gs://telus-kb-add-ons/{updated_id}.json\""
+    return updated_id
+
+# Reading and formatting data from product_spec_ids.txt
+formatted_product_spec_ids = []
+try:
+    with open("product_spec_ids.txt", "r") as file:
+        for line in file:
+            spec_id = line.strip()
+            formatted_product_spec_ids.append(format_product_spec_id(spec_id))
+except FileNotFoundError:
+    print("File product_spec_ids.txt not found.")
+
+# Reading and formatting data from updated_ids.txt
+formatted_updated_ids = []
+try:
+    with open("updated_ids.txt", "r") as file:
+        for line in file:
+            updated_id = line.strip()
+            formatted_updated_ids.append(format_updated_id(updated_id))
+except FileNotFoundError:
+    print("File updated_ids.txt not found.")
+
+# Writing formatted data to a new file
+with open("formatted_output.txt", "w") as output_file:
+    all_formatted_lines = formatted_product_spec_ids + formatted_updated_ids
+    for index, formatted_line in enumerate(all_formatted_lines):
+        # Adding a comma to all lines except the last one
+        if index < len(all_formatted_lines) - 1:
+            output_file.write(formatted_line + ",\n")
+        else:
+            output_file.write(formatted_line + "\n")
---
+++ script.py
@@ -0,0 +1,108 @@
+import os
+import json
+import subprocess
+import shutil
+
+# Directory where the files are located
+directory = 'Sample_JSON_30May23'
+
+# Request the input file name from the user
+input_file_name = input("Enter the name of the input file (without .json extension): ")
+input_file_path = os.path.join(directory, input_file_name + '.json')
+try:
+    with open(input_file_path, 'r') as file:
+        data = json.load(file)
+except FileNotFoundError:
+    print(f"File {input_file_path} not found.")
+    exit()
+except json.JSONDecodeError:
+    print(f"Error reading file {input_file_path}. Make sure it is a JSON file.")
+    exit()
+
+# Extracting the values of id from the bundledProductOffering array
+bundled_product_offerings = data.get('productOffering', {}).get('bundledProductOffering', [])
+updated_ids = []
+for product in bundled_product_offerings:
+    product_id = product.get('id', '')
+    if product_id:
+        # Searching for the corresponding file with a prefix in the Sample_JSON_30May23 directory
+        found = False
+        for file_name in os.listdir(directory):
+            if product_id in file_name:
+                prefix = file_name.split('_')[0]
+                updated_ids.append(prefix + '_' + product_id)
+                found = True
+                break
+        if not found:
+            updated_ids.append('G_' + product_id)
+
+# Writing the updated IDs to the updated_ids.txt file
+with open('updated_ids.txt', 'w') as file:
+    for updated_id in updated_ids:
+        file.write(updated_id + '\n')
+
+# Set to store unique productSpecification IDs with prefix
+unique_product_spec_ids = set()
+
+# Looping through each file name from updated_ids
+files_to_remove = []
+files_that_exist = set()
+for file_name in updated_ids:
+    # Building file path
+    file_path = os.path.join(directory, file_name + ".json")
+    
+    # Opening and loading JSON file
+    with open(input_file_path, "r") as currentfile:
+        current_file_data = json.load(currentfile)
+        currentdata = current_file_data["productOffering"].get("productSpecification", [])
+    
+    try:
+        with open(file_path, "r") as file:
+            file_data = json.load(file)
+            
+            # Getting the productSpecification array
+            product_specifications = file_data["productOffering"].get("productSpecification", []) + currentdata
+            
+            # Adding each ID with 'FTR_' prefix to the set
+            for product_spec in product_specifications:
+                spec_id = product_spec.get("id", "")
+                if spec_id:
+                    unique_product_spec_ids.add("FTR_" + spec_id)
+            files_that_exist.add(file_name.split('_')[-1])
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        files_to_remove.append(file_name)
+    except json.JSONDecodeError:
+        print(f"Error reading file {file_path}.")
+
+# Filter the bundledProductOffering array
+new_bundled_product_offerings = [
+    product for product in bundled_product_offerings if product.get('id') in files_that_exist
+]
+
+# Update the data
+data['productOffering']['bundledProductOffering'] = new_bundled_product_offerings
+
+# Create a copy of the original file
+shutil.copy(input_file_path, os.path.join(directory, input_file_name + '_copy.json'))
+
+# Save the modified data to the copy file
+copy_file_path = os.path.join(input_file_name + '_copy.json')
+with open(copy_file_path, 'w') as file:
+    json.dump(data, file, indent=2)
+
+# Removing non-existent files from updated_ids.txt
+updated_ids = [item for item in updated_ids if item not in files_to_remove]
+
+# Writing the updated IDs again to updated_ids.txt
+with open('updated_ids.txt', 'w') as file:
+    for updated_id in updated_ids:
+        file.write(updated_id + '\n')
+
+# Writing unique productSpecification IDs to a new file
+with open("product_spec_ids.txt", "w") as output_file:
+    for spec_id in unique_product_spec_ids:
+        output_file.write(spec_id + "\n")
+
+# Running the final_output.py script as a subprocess
+subprocess.run(["python", "final_output.py"])
